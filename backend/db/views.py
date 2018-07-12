95f6c588811153512f4df5b25d62960c1313c8f2
Unknown <crkdn@hnrhn.xyz>
2018-07-12 17:03:37 +0100

    Rename django folder to "backend" and fix weather request

--- backend/db/views.py
+++ backend/db/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+
 def current_weather(request):
     from db.models import DarkskyCurrentWeather as weather
     
@@ -8,7 +9,7 @@ def current_weather(request):
     # [:1] says we actually only want the first row
     # .values() converts from a QuerySet to a list of dicts (one dict per row)
     # [0] gets the first dict in the list (in this case we only have one
-    first_row_of_table_as_dict = weather.objects.all()[:1].values()[0]
+    first_row_of_table_as_dict = weather.objects.all().order_by("-timestamp")[:1].values()[0]
 
     # This automatically serialises the above to JSON and sends it as a HTTP response.
     return JsonResponse(first_row_of_table_as_dict)
