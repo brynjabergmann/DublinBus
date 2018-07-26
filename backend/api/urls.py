77ace23a8f489007807663e8989eb0691ae80dbc
09452893 <mariyana.levova@ucdconnect.ie>
2018-07-26 16:28:12 +0100

    added prediction method for a selected time

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("current_weather", views.current_weather, name="current_weather"),
     path("make_prediction", views.make_prediction, name="make_prediction"),
     path("stop_location", views.stop_location, name="stop_location"),
+    path("forecast", views.forecast, name="forecast"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -7,13 +7,28 @@ import pickle
 import pandas as pd
 import os
 import sklearn
-
+import datetime as dt
 
 def current_weather(request):
     from api.models import DarkskyCurrentWeather as weather
     first_row_of_table_as_dict = weather.objects.all().order_by("-timestamp")[:1].values()[0]
     return JsonResponse(first_row_of_table_as_dict)
 
+def forecast(request):
+    values = json.loads(request.body.decode('utf-8'))
+    hour = values["hour"]
+    day = dt.datetime.today().weekday()
+    
+    daily = []
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT dow, hour, temp, precip_intensity from DarkSky_hourly_weather_prediction WHERE dow = %s AND hour = %s;", [day, hour])
+        row = cursor.fetchone()
+        dow = row[0]
+        hour = row[1]
+        temp = row[2]
+        rain = row[3]
+        forecast = { "dow" : dow, "hour" : hour, "temp" : temp, "rain" : rain }   
+        return JsonResponse(forecast)
 
 @csrf_exempt    # Can remove in production, needed for testing
 def make_prediction(request):
