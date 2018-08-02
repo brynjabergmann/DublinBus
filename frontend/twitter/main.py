a4577904e42e52da425748febdd465c68fbb6859
Neil McKimm <neil.mckimm@ucdconnect.ie>
2018-08-02 11:19:06 +0100

    twitter script corrected

--- frontend/twitter/main.py
+++ frontend/twitter/main.py
@@ -1,27 +1,124 @@
 import twitter
 import json
 import time
+import re
+import pprint
+import numpy as np
 
 
-with open("twitter_auth.json") as f:
-    twitter_auth = json.loads(f.read())
-    consumer_key = twitter_auth["consumer_key"]
-    consumer_secret = twitter_auth["consumer_secret"]
-    access_token_key = twitter_auth["access_token_key"]
-    access_token_secret = twitter_auth["access_token_secret"]
+def banner_tweets():
 
-api = twitter.Api(consumer_key = consumer_key,
-                  consumer_secret = consumer_secret,
-                  access_token_key  = access_token_key,
-                  access_token_secret = access_token_secret)
+    with open("twitter_auth.json") as f:
+        twitter_auth = json.loads(f.read())
+        consumer_key = twitter_auth["consumer_key"]
+        consumer_secret = twitter_auth["consumer_secret"]
+        access_token_key = twitter_auth["access_token_key"]
+        access_token_secret = twitter_auth["access_token_secret"]
 
-api.VerifyCredentials()
-tweets = api.GetUserTimeline(screen_name='aaroadwatch', count=10,)
+    api = twitter.Api(consumer_key = consumer_key,
+                      consumer_secret = consumer_secret,
+                      access_token_key  = access_token_key,
+                      access_token_secret = access_token_secret)
 
-now = str(time.strftime("%a"))
-dublin_tweets = []
-i=0
-for t in tweets:
-    if t.text.startswith("#DUBLIN") and t.created_at.startswith(now):
-        dublin_tweets.append(t.text)
-    i += 1
+    api.VerifyCredentials()
+    tweets = api.GetUserTimeline(screen_name='aaroadwatch', count=15,)
+
+    now = str(time.strftime("%a"))
+    dublin_tweets = []
+    i=0
+    for t in tweets:
+        m = re.search('(?:)(cleared)(?:)', t.text)
+        if m:
+            pass
+        elif t.text.startswith("#DUBLIN") and t.created_at.startswith(now):
+            dublin_tweets.append(t.text)
+        else:
+            pass
+        i += 1
+    return dublin_tweets
+
+
+
+
+def banner_tweets_regex():
+
+    with open("twitter_auth.json") as f:
+        twitter_auth = json.loads(f.read())
+        consumer_key = twitter_auth["consumer_key"]
+        consumer_secret = twitter_auth["consumer_secret"]
+        access_token_key = twitter_auth["access_token_key"]
+        access_token_secret = twitter_auth["access_token_secret"]
+
+    api = twitter.Api(consumer_key = consumer_key,
+                      consumer_secret = consumer_secret,
+                      access_token_key  = access_token_key,
+                      access_token_secret = access_token_secret)
+
+    api.VerifyCredentials()
+    tweets = api.GetUserTimeline(screen_name='aaroadwatch', count=150,)
+
+    now = str(time.strftime("%a"))
+    dublin_tweets_raw = []
+    dublin_tweets_lower = []
+    fixed_traffic = []
+    i=0
+    current_day = str(time.strftime("%a"))
+
+    for t in tweets:
+        m = re.search('(?:)((C|c)leared|((n|N)(o|O) (F|f)urther)|(R|r)eopened)|((M|m)oved)(?:)', t.text)
+        if m and t.text.startswith("#DUBLIN") and t.created_at.startswith(current_day):
+            fixed_traffic.append(t.text)
+        elif t.text.startswith("#DUBLIN") and t.created_at.startswith(current_day):
+            dublin_tweets_lower.append(t.text.lower())
+            dublin_tweets_raw.append(t.text)
+        else:
+            pass
+        i += 1
+    #roads_regex = re.findall('?:((on the)|(at the)|(\bon)|(\bat)|(i\bn)))(.*)(?:(\. More)|(\.)', dublin_tweets_raw[1])
+    roads = []
+    m_roads = []
+    n_roads = []
+    m_test = np.empty()
+    tt = np.empty(5, 5, dtype=string, order='C')
+    pprint.pprint(tt)
+    for i in range(len(dublin_tweets_raw)):
+        n_regex = re.search(r'\bon the\b|\bon\b ((N\d+|N\d+\/M\d+)(?:.*)(J\d+)*)', dublin_tweets_raw[i])
+        m_regex = re.search(
+            r'\bon the\b|\bon\b ((N\d+|M50)\s+(northbound|southbound|at)(?:\.|\s+|\s+at\s+|.*)(J\d+)*)\.\s+More|\.',
+            dublin_tweets_raw[i])
+        roads_regex = re.search(r'(?:\bon the\b\s|\bon\b\s)(.*)\. More|\.', dublin_tweets_raw[i])
+        test = "#DUBLIN Incident on M50 at J6. More here: https://t.co/PSzIBsvOzE"
+
+
+        print(dublin_tweets_raw[i])
+        if n_regex and n_regex.group(1):
+            n_roads.append(n_regex.group(0))
+            # n_regex.group()
+            # 1 = whole matched statement
+            # 2 = road name
+            # 4 = junction
+        elif m_regex.group(1):
+            # m_regex.group()
+            # 1 = whole matched statement e.g. M50 southbound at J6
+            # 2 = road name e.g. M50
+            # 3 = direction e.g southbound
+            # 4 = junction
+            m_roads.append(m_regex.group(2))
+            m_test[i].append(m_regex.group(2))
+            m_test[i].append(m_regex.group(3))
+            m_test[i].append(m_regex.group(4))
+            m_test[i].append(m_regex.group(1))
+        elif roads_regex.group(1):
+            roads.append(roads_regex.group(1))
+        else:
+            pass
+    pprint.pprint(dublin_tweets_raw)
+    pprint.pprint(fixed_traffic)
+    pprint.pprint(roads)
+    pprint.pprint(m50)
+    pprint.pprint(n_roads)
+
+    return roads
+# fixed tweets and roads > contained in then delete both
+
+banner_tweets_regex()
