57680b66ca26ff7577b1e7fe7e78306997833c4a
Unknown <ptr@hnrhn.xyz>
2018-08-06 23:52:27 +0100

    [UNDO BEFORE MERGE] Remove url_patterns

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("current_weather", views.current_weather, name="current_weather"),
-    path("make_prediction", views.make_prediction, name="make_prediction"),
-    path("stop_location", views.stop_location, name="stop_location"),
-    path("forecast", views.forecast, name="forecast"),
-    path("daily_forecast", views.daily_forecast, name="daily_forecast"),
-    path("stop_timer", views.stop_timer, name="stop_timer"),
-]
+urlpatterns = []
+#     path("current_weather", views.current_weather, name="current_weather"),
+#     path("make_prediction", views.make_prediction, name="make_prediction"),
+#     path("stop_location", views.stop_location, name="stop_location"),
+#     path("forecast", views.forecast, name="forecast"),
+#     path("daily_forecast", views.daily_forecast, name="daily_forecast"),
+#     path("stop_timer", views.stop_timer, name="stop_timer"),
+# ]
