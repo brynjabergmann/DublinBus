67d68fe16027211edc11310f0dbe0cfd5b1ffbe1
Unknown <ptr@hnrhn.xyz>
2018-08-13 15:12:48 +0100

    Merge branch 'design' into dev

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("predict", views.prediction_endpoint, name="predict"),
     path("current_weather", views.current_weather_endpoint, name="current_weather"),
     path("chart", views.chart_endpoint, name="chart")
+    path("make_prediction_using_coordinates", views.make_prediction_using_coordinates, name="make_prediction_using_coordinates"),
 ]
