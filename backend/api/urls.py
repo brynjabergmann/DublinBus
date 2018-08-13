8503228fc9dd92a55d7b4ae8dc16205a7f73e95e
Unknown <ptr@hnrhn.xyz>
2018-08-13 15:19:49 +0100

    Fix typo in paths list

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("predict", views.prediction_endpoint, name="predict"),
     path("current_weather", views.current_weather_endpoint, name="current_weather"),
-    path("chart", views.chart_endpoint, name="chart")
+    path("chart", views.chart_endpoint, name="chart"),
     path("make_prediction_using_coordinates", views.make_prediction_using_coordinates, name="make_prediction_using_coordinates"),
     path("make_prediction_using_coordinates", views.make_prediction_using_coordinates, name="make_prediction_using_coordinates"),
 ]
