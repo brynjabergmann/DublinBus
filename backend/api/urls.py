411a43b428ef6af44e1f47fc0c0407e8c7d21d58
Peter Hanrahan <ptr@hnrhn.xyz>
2018-08-17 22:27:33 +0100

    Improved location-based predictions & charts

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("predict", views.prediction_endpoint, name="predict"),
+    path("old_predict", views.prediction_endpoint, name="old_predict"),
     path("current_weather", views.current_weather_endpoint, name="current_weather"),
     path("chart", views.chart_endpoint, name="chart"),
-    path("location-prediction", views.predict_from_locations, name="location_prediction"),
+    path("location-prediction", views.predict, name="location_prediction"),
     path("location_prediction_endpoint", views.location_prediction_endpoint, name="location_prediction_endpoint"),
     path("roadwatch", views.roadwatch_endpoint, name="roadwatch")
 ]
